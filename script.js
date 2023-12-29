a03242564f1c359551cc354e2cbae75822fdbbf6
ornob90 <oornob49@gmail.com>
2023-12-29 22:03:57 +0600

    implement search functionality

--- script.js
+++ script.js
@@ -1,10 +1,30 @@
 const collection = document.getElementById("collection");
-
+const search = document.getElementById("search");
 import blogsData from "./blogs-data.js";
 
-const addBlogs = () => {
-  console.log("hitted");
-  blogsData.forEach((blog) => {
+const blogs = [...blogsData];
+
+// Functions
+
+const handleSearch = (e) => {
+  if (!e.target.value) {
+    addBlogs(blogsData);
+    return;
+  }
+
+  const filteredBlogs = blogs.filter(({ title }) =>
+    title.toLowerCase().includes(e.target.value.toLowerCase())
+  );
+
+  console.log(filteredBlogs);
+
+  addBlogs(filteredBlogs);
+};
+
+const addBlogs = (blogs) => {
+  //   console.log("hitted");
+  collection.innerHTML = "";
+  blogs.forEach((blog) => {
     const card = document.createElement("div");
 
     card.classList.add("min-h-[500px]", "flex", "flex-col", "gap-4");
@@ -41,4 +61,7 @@ const addBlogs = () => {
   });
 };
 
-addBlogs();
+addBlogs(blogs);
+
+// Event Listeners
+search.addEventListener("input", handleSearch);
